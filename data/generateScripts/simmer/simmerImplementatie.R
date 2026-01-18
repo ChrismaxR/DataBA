67c4f4bae7278cca98f33060a8517ca834fc054f
Christian van Gaalen <ctmvangaalen@gmail.com>
2026-01-18 07:30:48 +0100

    simpele branch werkt in simmer env + testscript voor simmer

--- data/generateScripts/simmer/simmerImplementatie.R
+++ data/generateScripts/simmer/simmerImplementatie.R
@@ -17,64 +17,98 @@ to_datetime <- function(t_minutes) {
 
 # 1) Simulatie-omgeving + resources
 env <- simmer("hospital") |>
-    add_resource("receptie", capacity = 3, queue_size = Inf) |>
-    add_resource("diagnose", capacity = 2, queue_size = Inf) |>
-    add_resource("behandeling", capacity = 1, queue_size = Inf)
+    add_resource("Receptie", capacity = 3, queue_size = Inf) |>
+    add_resource("The GP's office", capacity = 2, queue_size = Inf) |>
+    add_resource("General Diagnosis Room", capacity = 2, queue_size = Inf) |>
+    add_resource("The Ward", capacity = 2, queue_size = Inf) |>
+    add_resource("Slack Tongue Clinic", capacity = 1, queue_size = Inf)
 
-# 2) Patiënt-traject (super simpel)
-patient <- trajectory("patient") |>
-    log_("aankomst") |>
-    seize("receptie", 1) |>
-    log_("inReceptie", level = 2) |>
-    timeout(function() runif(1, 5, 10)) |> # minuten
-    release("receptie", 1, ) |>
-    log_("inWachtrijDiagnose") |>
-    seize("diagnose", 1) |>
+# 2) Patient subtrajectories
+
+trajA <- trajectory("A") |>
+    seize("The GP's office", 1) |>
     timeout(function() runif(1, 10, 20)) |>
-    release("diagnose", 1) |>
-    log_("inWachtrijBehandeling") |>
-    seize("behandeling", 1) |>
+    release("The GP's office", 1) |>
+    seize("The Ward", 1) |>
     timeout(function() runif(1, 15, 30)) |>
-    release("behandeling", 1)
+    release("The Ward", 1)
+
+trajB <- trajectory("B") |>
+    seize("General Diagnosis Room", 1) |>
+    timeout(function() runif(1, 10, 20)) |>
+    release("General Diagnosis Room", 1) |>
+    seize("Slack Tongue Clinic", 1) |>
+    timeout(function() runif(1, 15, 30)) |>
+    release("Slack Tongue Clinic", 1)
+
+traj0 <- trajectory("0") |>
+    set_attribute(keys = "aandoeningId", values = function() {
+        sample(1L:2L, 1)
+    }) |>
+    log_(function() {
+        paste(
+            "Aankomst met aandoeningId = ",
+            get_attribute(env, "aandoeningId")
+        )
+    }) |>
+    seize("Receptie", 1) |>
+    timeout(function() runif(1, 5, 10)) |> # minutes
+    release("Receptie", 1) %>%
+    branch(
+        function() {
+            r <- get_attribute(env, "aandoeningId")
+            if (r %in% c(1)) {
+                1L # -> traj_A
+            } else if (r %in% c(2)) {
+                2L # -> traj_B
+            } else {
+                3L # -> traj_C
+            }
+        },
+        trajA,
+        trajB,
+        continue = TRUE
+    ) |>
+    log_(function() {
+        paste("Ontslag met aandoeningId = ", get_attribute(env, "aandoeningId"))
+    })
 
 # 3) Generator: patiënten komen binnen (bv elke 3-8 minuten)
 env |>
     add_generator(
         name_prefix = "patient",
-        trajectory = patient,
-        distribution = function() runif(1, 1, 2),
+        trajectory = traj0,
+        distribution = function() runif(1, 1, 5),
         mon = 2
     ) |>
-    run(until = 8 * 60) # 2 uur simulatie
+    run(until = 1 * 60) # aantal uur simulatie
 
-# 4) Monitoring: wie zat wanneer op welke resource
-events <- get_mon_arrivals(env, per_resource = TRUE)
+# 5) Monitoring: wie zat wanneer op welke resource
+get_mon_arrivals(env, per_resource = TRUE)
+get_mon_attributes(env)
+get_mon_resources(env)
 
-## deze
-mon_resources <- get_mon_resources(env)
-attr_mon <- get_mon_attributes(env)
-
-# 5) Ombouwen van monitoring data naar event tabel
+# 6) Ombouwen van monitoring data naar event tabel
 # NB get_mon_arrivals() geeft niet direct een queue time, dit moet ik berekenen:
 # waiting_time_next_event = end_time_this_event - (start_time_this_event + activity_time_this_event)
-events <- events |>
-    #filter(name == "patient10") |>
-    transmute(
-        name,
-        resource,
-        start_time_this_event = start_time,
-        activity_time_this_event = activity_time,
-        end_time_this_event = end_time,
-        release_time_this_event = start_time_this_event +
-            activity_time_this_event,
-        wait_time_next_resource = end_time_this_event - release_time_this_event,
-        seize_time_next_resource = end_time
-        #patientId = as.integer(gsub("\\D+", "", name)),
-        #kamerType = resource,
-        #start_dt = to_datetime(start_time),
-        #end_dt = to_datetime(end_time),
-        #activity_time = difftime(end_dt, start_dt, units = "mins"),
-        #eventType = "inGebruik"
-    ) |>
-    # maak tabel long
-    tidyr::pivot_longer(cols = 3:7, names_repair = "minimal")
+# events <- events |>
+#filter(name == "patient10") |>
+#    transmute(
+#        name,
+#        resource,
+#        start_time_this_event = start_time,
+#        activity_time_this_event = activity_time,
+#        end_time_this_event = end_time,
+#        release_time_this_event = start_time_this_event +
+#            activity_time_this_event,
+#        wait_time_next_resource = end_time_this_event - release_time_this_event,
+#        seize_time_next_resource = end_time
+#        #patientId = as.integer(gsub("\\D+", "", name)),
+#        #kamerType = resource,
+#        #start_dt = to_datetime(start_time),
+#        #end_dt = to_datetime(end_time),
+#        #activity_time = difftime(end_dt, start_dt, units = "mins"),
+#        #eventType = "inGebruik"
+#    ) |>
+#    # maak tabel long
+#    tidyr::pivot_longer(cols = 3:7, names_repair = "minimal")
---
+++ data/generateScripts/simmer/simmerTest.R
@@ -0,0 +1,22 @@
+library(simmer)
+env <- simmer()
+
+traj <- trajectory() %>%
+  set_global("path", 1, mod = "+", init = -1) %>%
+  log_(function() paste("Path", get_global(env, "path"), "selected")) %>%
+  branch(
+    function() get_global(env, "path"),
+    continue = c(TRUE, FALSE),
+    trajectory() %>%
+      log_("following path 1"),
+    trajectory() %>%
+      log_("following path 2"),
+    trajectory() |>
+      log_("following path 3")
+  ) %>%
+  log_("continuing after the branch (path 0)")
+
+env %>%
+  add_generator("dummy", traj, at(0:3)) %>%
+  run() %>%
+  invisible
